dfbc677b546a096c358ac42937bb4c7dbaf81c6d
tisha39 <nahidabhuyan39@gmail.com>
2024-09-14 00:10:02 +0600

    first commit

---
+++ day01.js
@@ -0,0 +1,21 @@
+// strings
+
+var name ="nahida bhuyan"
+var address ="kishoreganj"
+var city ="kishre"
+var country='bangladesh dhaka kishoreganj sodor '
+var husband='abir khan'
+console.log(husband)
+
+// boolean 
+var isRich=true;
+var passed=false;
+
+console.log( passed)
+
+
+
+
+
+
+
